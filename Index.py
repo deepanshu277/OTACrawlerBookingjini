857a569477fb8aca6d74f00e8d26a8a9eb2109b7
Rajatav Dutta <rajatavdutta.pro@gmail.com>
2020-01-05 11:18:35 +0530

    Update Index.py
    updated the IP proxy algo

--- Index.py
+++ Index.py
@@ -5,6 +5,34 @@ from selenium import webdriver
 from pprint import pprint
 from xvfbwrapper import Xvfb
 from random import randrange
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import random
+
+ua = UserAgent() # From here we generate a random user agent
+proxies = [] # Will contain proxies [ip, port]
+
+proxies_req = Request('https://www.sslproxies.org/')
+proxies_req.add_header('User-Agent', ua.random)
+proxies_doc = urlopen(proxies_req).read().decode('utf8')
+
+soup = BeautifulSoup(proxies_doc, 'html.parser')
+proxies_table = soup.find(id='proxylisttable')
+
+# Save proxies in the array
+for row in proxies_table.tbody.find_all('tr'):
+    proxies.append({
+      'ip':   row.find_all('td')[0].string,
+      'port': row.find_all('td')[1].string
+    })
+
+def random_proxy():
+  return random.randint(0, len(proxies) - 1)
+
+# Choose a random proxy
+proxy_index = random_proxy()
+proxy = proxies[proxy_index]    
 
 def parse(url):
     searchKey = "Bhubaneswar" # Change this to your city 
