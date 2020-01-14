cd06f6c3bdc9957711e19846fcb56cc2538e8ccd
Rajatav Dutta <rajatavdutta.pro@gmail.com>
2020-01-15 03:41:47 +0530

    Update expedia.py

--- expedia.py
+++ expedia.py
@@ -10,7 +10,7 @@ def parse(url):
     searchKey = "Haldia" # Change this to your city 
     checkInDate = '16/01/2020' #Format %d/%m/%Y
     checkOutDate = '17/01/2020' #Format %d/%m/%Y
-    response = webdriver.Chrome(r'C:\Users\KIIT\Downloads\chromedriver.exe')
+    response = webdriver.Chrome(r'C:\Users\TripleR\Downloads\chromedriver_win32\chromedriver.exe')
     response.get(url)
     searchKeyElement = response.find_elements_by_xpath('//*[@id="hotel-destination-hp-hotel"]')
     checkInElement = response.find_elements_by_xpath('//*[@id="hotel-checkin-hp-hotel"]')
@@ -40,11 +40,12 @@ def parse(url):
     sleep(5)
     #response.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     parser = html.fromstring(response.page_source,response.current_url)
-    hotels = parser.xpath('//*[@id="app"]/div[1]/div/div/div/div[1]/main/div/section[2]/ol/li[1]/div/div')
+    hotels = parser.xpath('//div[@class="uitk-card-content uitk-grid uitk-cell all-y-padding-three all-x-padding-three listing-content"]')
+    a = 0
     for hotel in hotels[:]: #Replace 5 with 1 to just get the cheapest hotel
-        hotelName = hotel.xpath('./h3')
+        hotelName = hotel.xpath('.//h3')
         hotelName = hotelName[0].text_content() if hotelName else None
-        price = hotel.xpath('//*[@id="app"]/div[1]/div/div/div/div[1]/main/div/section[2]/ol/li[2]/div/div/div/div[2]/div[1]/div[1]/span/span[2]')
+        price = hotel.xpath('.//div[2]/div[1]/div[1]/span/span[2]')
         price = price[0].text_content().replace(",","").strip() if price else None
         if price==None:
             price = hotel.xpath('//*[@id="app"]/div[1]/div/div/div/div[1]/main/div/section[2]/ol/li[1]/div/div/div/div[2]/div[1]/div[1]/span/span[2]')
@@ -58,5 +59,6 @@ def parse(url):
                     "price":price
         }
         pprint(item)
-
+        a = a + 1
+    print(a)
 parse('https://www.expedia.co.in')
